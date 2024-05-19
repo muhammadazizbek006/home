fbb8cb5556e78c4b09db63a93399e050cbcfb40c
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-19 16:41:18 +0300

    add haida past

--- src/components/home/KompaniyaXaqida.jsx
+++ src/components/home/KompaniyaXaqida.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+// data
+import {haqida} from '../../Data'
 const KompaniyaXaqida = () => {
   return (
     <>
         <section className='py-20'>
-            <div className="containerb pt-20 pb-16 px-16  bg-black rounded-2xl text-white">
+            <div className="containerb w-[1651px] pt-20 pb-16 pl-20   bg-black rounded-2xl text-white">
                   <h2 className='text-6xl mb-14  '>О компании</h2>
                   {/* tepa */}
                   <div className='flex justify-between mb-20'>
@@ -15,15 +16,29 @@ const KompaniyaXaqida = () => {
                       <p>Мы располагаем большим арсеналом готовых проектов. С радостью вносим изменения в уже имеющийся проект. Если Вас не устроит наш вариант, специалисты нашей компании разработают индивидуальный проект специально для Вас. </p>
                     </div>
                     {/* right */}
-                    <div>
+                    <div className='mr-48'>
                       <p className='text-2xl mb-10'><b className='text-6xl'>более 100</b> <br /> проектов за все время</p>
                       <p className='text-2xl'><b className='text-6xl'>более 550</b> <br /> семей доверили нам <br /> свой дом</p>
 
                     </div>
                   </div>
                   {/* past */}
-                  <ul>
-                    
+                  <ul className='grid grid-cols-3 gap-x-6'>
+                    {
+                      haqida.map((e)=>{
+                        return(
+                          <li key={e.id} className='text-white w-[494px] flex flex-col border-2 pl-12 pt-16 border-gren hover:bg-gren  duration-500 rounded-2xl'>
+                            <div >
+                              <img className='p-4 bg-gren rounded-lg mb-8' src={e.img} alt={e.ustunlik} />
+                            </div>
+                            <div className=' max-w-80 '>
+                              <h3 className='text-2xl mb-4 '>{e.ustunlik}</h3>
+                              <p className='text-xl mb-16'>{e.sharx}</p>
+                            </div>
+                          </li>
+                        )
+                      })
+                    }
                   </ul>
             </div>
         </section>
