3463ec4e9ff5d93482926ed6dfe15e25233f5d89
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-05-19 16:23:39 +0300

    add haqida tepa bit di

--- src/components/home/KompaniyaXaqida.jsx
+++ src/components/home/KompaniyaXaqida.jsx
@@ -3,9 +3,28 @@ import React from 'react'
 const KompaniyaXaqida = () => {
   return (
     <>
-        <section>
-            <div className="containerb">
-                
+        <section className='py-20'>
+            <div className="containerb pt-20 pb-16 px-16  bg-black rounded-2xl text-white">
+                  <h2 className='text-6xl mb-14  '>О компании</h2>
+                  {/* tepa */}
+                  <div className='flex justify-between mb-20'>
+                    {/* left */}
+                    <div className='text-2xl max-w-[955px]'>
+                      <p className='mb-12'>Наша компания занимается проектированием и строительством загородных домов, коттеджей и бань из профилированного бруса. Для сооружения всех конструкций мы используем профилированный брус — экологически чистый, натуральный строительный материал.</p>
+
+                      <p>Мы располагаем большим арсеналом готовых проектов. С радостью вносим изменения в уже имеющийся проект. Если Вас не устроит наш вариант, специалисты нашей компании разработают индивидуальный проект специально для Вас. </p>
+                    </div>
+                    {/* right */}
+                    <div>
+                      <p className='text-2xl mb-10'><b className='text-6xl'>более 100</b> <br /> проектов за все время</p>
+                      <p className='text-2xl'><b className='text-6xl'>более 550</b> <br /> семей доверили нам <br /> свой дом</p>
+
+                    </div>
+                  </div>
+                  {/* past */}
+                  <ul>
+                    
+                  </ul>
             </div>
         </section>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero from '../components/home/Hero'
-
+import KlinikaXaqida from '../components/home/KompaniyaXaqida'
 const Home = () => {
   return (
     <>
       <Hero/>
+      <KlinikaXaqida/>
     </>
   )
 }
